d1e281a42b5096dfbf61cb3474f8a195a5705a0f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-13 23:20:56 +0900

    Add more tests

--- tests/minheap/tests.rb
+++ tests/minheap/tests.rb
@@ -1,4 +1,13 @@
 def test_minheap_initialize(_args, assert)
-  MinHeap.new
-  assert.ok!
+  heap = MinHeap.new(8, 1, 3)
+
+  assert.equal! heap.peek, 1
+  assert.equal! heap.pop, 1
+  assert.equal! heap.peek, 3
+  assert.equal! heap.pop, 3
+  assert.equal! heap.peek, 8
+  assert.equal! heap.pop, 8
+  assert.equal! heap.peek, nil
+  assert.equal! heap.pop, nil
 end
+
