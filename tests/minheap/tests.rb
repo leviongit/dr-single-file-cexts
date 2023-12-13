09f5da07f662caa596cdb5366af49d5f40cb8fd0
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-13 23:25:24 +0900

    Add test for arrays/custom object

--- tests/minheap/tests.rb
+++ tests/minheap/tests.rb
@@ -11,3 +11,14 @@ def test_minheap_initialize(_args, assert)
   assert.equal! heap.pop, nil
 end
 
+def test_minheap_custom_objects(_args, assert)
+  heap = MinHeap.new
+
+  heap.insert [10, 'Bob']
+  heap.insert [5, :red]
+  heap.insert [7, { hp: 100 }]
+
+  assert.equal! heap.pop, [5, :red]
+  assert.equal! heap.pop, [7, { hp: 100 }]
+  assert.equal! heap.pop, [10, 'Bob']
+end
