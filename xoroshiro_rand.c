43861d51951a89ef5820c7e01cc13b7f1aec3ee5
leviongit <levi.duncan.self@gmail.com>
2024-10-09 04:33:31 +0200

    the algo is xoroshiro128+ (only one `+`)

--- xoroshiro_rand.c
+++ xoroshiro_rand.c
@@ -7,19 +7,19 @@
 #include <stdint.h>
 #include <string.h>
 
-/* xoroshiro128++ [https://prng.di.unimi.it/xoroshiro128plus.c] */
+/* xoroshiro128+ [https://prng.di.unimi.it/xoroshiro128plus.c] */
 static inline uint64_t rotl(const uint64_t x, const uint8_t k) {
   return (x << k) | (x >> (64 - k));
 }
 
-struct xoroshiro128pp_st {
+struct xoroshiro128p_st {
   uint64_t lo;
   uint64_t hi;
 };
 
-static_assert(sizeof(struct xoroshiro128pp_st) <= ISTRUCT_DATA_SIZE);
+static_assert(sizeof(struct xoroshiro128p_st) <= ISTRUCT_DATA_SIZE);
 
-uint64_t xoroshiro128pp_next(struct xoroshiro128pp_st *st) {
+uint64_t xoroshiro128p_next(struct xoroshiro128p_st *st) {
   const uint64_t s0 = st->lo;
   uint64_t s1 = st->hi;
 
@@ -32,7 +32,7 @@ uint64_t xoroshiro128pp_next(struct xoroshiro128pp_st *st) {
   return res;
 }
 
-void xoroshiro128pp_jump(struct xoroshiro128pp_st *st) {
+void xoroshiro128p_jump(struct xoroshiro128p_st *st) {
   static const uint64_t JUMP[] = {0x2bd7a6a6e99c2ddc, 0x0992ccaf6a6fca05};
 
   uint64_t s0 = 0;
@@ -43,14 +43,14 @@ void xoroshiro128pp_jump(struct xoroshiro128pp_st *st) {
         s0 ^= st->lo;
         s1 ^= st->hi;
       }
-      xoroshiro128pp_next(st);
+      xoroshiro128p_next(st);
     }
 
   st->lo = s0;
   st->hi = s1;
 }
 
-void xoroshiro128pp_long_jump(struct xoroshiro128pp_st *st) {
+void xoroshiro128p_long_jump(struct xoroshiro128p_st *st) {
   static const uint64_t LONG_JUMP[] = {0x360fd5f2cf8d5d99, 0x9c6e6877736c46e3};
 
   uint64_t s0 = 0;
@@ -61,32 +61,32 @@ void xoroshiro128pp_long_jump(struct xoroshiro128pp_st *st) {
         s0 ^= st->lo;
         s1 ^= st->hi;
       }
-      xoroshiro128pp_next(st);
+      xoroshiro128p_next(st);
     }
 
   st->lo = s0;
   st->hi = s1;
 }
 
-void xoroshiro128pp_init(struct xoroshiro128pp_st *st, uint64_t seed) {
-  *st = (struct xoroshiro128pp_st){
+void xoroshiro128p_init(struct xoroshiro128p_st *st, uint64_t seed) {
+  *st = (struct xoroshiro128p_st){
       .hi = rotl(seed ^ UINT64_C(0xfac1e04741dab55a), seed & 0x1f),
       .lo = rotl(seed, 12) ^ UINT64_C(0xf01e46382d57cab9)};
 }
 
-double xoroshiro128pp_next_float(struct xoroshiro128pp_st *st) {
-  return (xoroshiro128pp_next(st) & ~UINT64_C(1)) / (double)UINT64_MAX;
+double xoroshiro128p_next_float(struct xoroshiro128p_st *st) {
+  return (xoroshiro128p_next(st) & ~UINT64_C(1)) / (double)UINT64_MAX;
 }
 
-_Bool xoroshiro128pp_next_bool(struct xoroshiro128pp_st *st) {
-  return xoroshiro128pp_next(st) >> 63;
+_Bool xoroshiro128p_next_bool(struct xoroshiro128p_st *st) {
+  return xoroshiro128p_next(st) >> 63;
 }
 
-struct RClass *xoroshiro128pp;
+struct RClass *xoroshiro128p;
 
 mrb_value xoro_rand_alloc(mrb_state *mrb, mrb_value) {
   struct RIStruct *ris =
-      (struct RIStruct *)mrb_obj_alloc(mrb, MRB_TT_ISTRUCT, xoroshiro128pp);
+      (struct RIStruct *)mrb_obj_alloc(mrb, MRB_TT_ISTRUCT, xoroshiro128p);
   mrb_value val = mrb_obj_value(ris);
 
   memset(ris->inline_data, 0, sizeof(ris->inline_data));
@@ -99,31 +99,31 @@ mrb_value xoro_rand_new(mrb_state *mrb, mrb_value) {
   mrb_get_args(mrb, "|i", &seed);
 
   struct RIStruct *ris =
-      (struct RIStruct *)mrb_obj_alloc(mrb, MRB_TT_ISTRUCT, xoroshiro128pp);
+      (struct RIStruct *)mrb_obj_alloc(mrb, MRB_TT_ISTRUCT, xoroshiro128p);
   mrb_value val = mrb_obj_value(ris);
 
-  xoroshiro128pp_init((struct xoroshiro128pp_st *)&ris->inline_data, seed);
+  xoroshiro128p_init((struct xoroshiro128p_st *)&ris->inline_data, seed);
 
   return val;
 }
 
 mrb_value xoro_rand_init(mrb_state *mrb, mrb_value self) {
-  if (!mrb_obj_is_instance_of(mrb, self, xoroshiro128pp)) {
-    mrb_raisef(mrb, E_TYPE_ERROR, "%C::initialize called on %T", xoroshiro128pp,
+  if (!mrb_obj_is_instance_of(mrb, self, xoroshiro128p)) {
+    mrb_raisef(mrb, E_TYPE_ERROR, "%C::initialize called on %T", xoroshiro128p,
                self);
   }
 
   mrb_int seed = 0;
   mrb_get_args(mrb, "|i", &seed);
 
-  xoroshiro128pp_init((struct xoroshiro128pp_st *)ISTRUCT_PTR(self), seed);
+  xoroshiro128p_init((struct xoroshiro128p_st *)ISTRUCT_PTR(self), seed);
 
   return self;
 }
 
 mrb_value xoro_rand_rand(mrb_state *mrb, mrb_value self) {
   mrb_value arg = mrb_int_value(mrb, 0);
-  struct xoroshiro128pp_st *st = (typeof(st))ISTRUCT_PTR(self);
+  struct xoroshiro128p_st *st = (typeof(st))ISTRUCT_PTR(self);
 
   mrb_get_args(mrb, "|o", &arg);
 
@@ -139,9 +139,9 @@ mrb_value xoro_rand_rand(mrb_state *mrb, mrb_value self) {
                  self);
 
     if (i == 0)
-      return mrb_float_value(mrb, xoroshiro128pp_next_float(st));
+      return mrb_float_value(mrb, xoroshiro128p_next_float(st));
 
-    return mrb_int_value(mrb, xoroshiro128pp_next(st) % i);
+    return mrb_int_value(mrb, xoroshiro128p_next(st) % i);
 
     mrb_raise(mrb, E_RUNTIME_ERROR, "\"unreachable\" control flow");
   }
@@ -168,7 +168,7 @@ mrb_value xoro_rand_rand(mrb_state *mrb, mrb_value self) {
       if (span == 0)
         return mrb_nil_value();
 
-      mrb_int offs = xoroshiro128pp_next(st) % span;
+      mrb_int offs = xoroshiro128p_next(st) % span;
       return mrb_int_value(mrb, ia + offs);
     }
 
@@ -180,7 +180,7 @@ mrb_value xoro_rand_rand(mrb_state *mrb, mrb_value self) {
     if (span == 0.0)
       return mrb_nil_value();
 
-    return mrb_float_value(mrb, fa + (xoroshiro128pp_next_float(st) * span));
+    return mrb_float_value(mrb, fa + (xoroshiro128p_next_float(st) * span));
   }
 
   mrb_raisef(mrb, E_ARGUMENT_ERROR, "%v is not a valid %T#rand argument", arg,
@@ -189,24 +189,24 @@ mrb_value xoro_rand_rand(mrb_state *mrb, mrb_value self) {
 
 mrb_value xoro_rand_rand_bool([[maybe_unused]] mrb_state *mrb, mrb_value self) {
   return mrb_bool_value(
-      xoroshiro128pp_next_bool((struct xoroshiro128pp_st *)ISTRUCT_PTR(self)));
+      xoroshiro128p_next_bool((struct xoroshiro128p_st *)ISTRUCT_PTR(self)));
 }
 
 void drb_register_c_extensions_with_api(mrb_state *mrb, void *) {
-  xoroshiro128pp = mrb_define_class_id(mrb, mrb_intern_lit(mrb, "Xoroshiro128"),
+  xoroshiro128p = mrb_define_class_id(mrb, mrb_intern_lit(mrb, "Xoroshiro128"),
                                        mrb->object_class);
-  mrb_define_class_method_id(mrb, xoroshiro128pp,
+  mrb_define_class_method_id(mrb, xoroshiro128p,
                              mrb_intern_lit(mrb, "allocate"), xoro_rand_alloc,
                              MRB_ARGS_NONE());
-  mrb_define_class_method_id(mrb, xoroshiro128pp, mrb_intern_lit(mrb, "new"),
+  mrb_define_class_method_id(mrb, xoroshiro128p, mrb_intern_lit(mrb, "new"),
                              xoro_rand_new, MRB_ARGS_OPT(1));
-  mrb_define_method_id(mrb, xoroshiro128pp, mrb_intern_lit(mrb, "initialize"),
+  mrb_define_method_id(mrb, xoroshiro128p, mrb_intern_lit(mrb, "initialize"),
                        xoro_rand_init, MRB_ARGS_OPT(1));
 
-  mrb_define_method_id(mrb, xoroshiro128pp, mrb_intern_lit(mrb, "rand"),
+  mrb_define_method_id(mrb, xoroshiro128p, mrb_intern_lit(mrb, "rand"),
                        xoro_rand_rand, MRB_ARGS_OPT(1));
-  mrb_define_method_id(mrb, xoroshiro128pp, mrb_intern_lit(mrb, "rand_bool"),
+  mrb_define_method_id(mrb, xoroshiro128p, mrb_intern_lit(mrb, "rand_bool"),
                        xoro_rand_rand_bool, MRB_ARGS_NONE());
 
-  MRB_SET_INSTANCE_TT(xoroshiro128pp, MRB_TT_ISTRUCT);
+  MRB_SET_INSTANCE_TT(xoroshiro128p, MRB_TT_ISTRUCT);
 }
